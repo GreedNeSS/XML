6810b83a838220a9c6e4f51fadd89c70757d42b5
GreedNeSS <ruslan199126@gmail.com>
2022-03-13 23:32:07 +0300

    Added simple example

--- SimpleXML/Program.cs
+++ SimpleXML/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Xml;
 
 namespace SimpleXML
 {
@@ -6,7 +8,43 @@ namespace SimpleXML
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Simple XML *****");
+
+            List<Person> people = new List<Person>();
+
+            XmlDocument xml = new XmlDocument();
+            xml.Load(@".\..\..\..\People.xml");
+            XmlElement? xRoot = xml.DocumentElement;
+
+            if (xRoot != null)
+            {
+                foreach (XmlElement node in xRoot)
+                {
+                    //XmlNode atr = node.Attributes.GetNamedItem("name");
+                    //string atrVal = atr.Value;
+                    string name = node.GetAttribute("name");
+
+                    Person person = new() { Name = name };
+
+                    foreach (XmlNode field in node.ChildNodes)
+                    {
+                        if (field.Name == "age")
+                            person.Age = int.Parse(field.InnerText);
+                        
+                        if (field.Name == "company")
+                            person.Company = field.InnerText;
+                    }
+
+                    people.Add(person);
+                }
+            }
+
+            Console.WriteLine("\n=> people:");
+
+            foreach (Person per in people)
+            {
+                Console.WriteLine(per);
+            }
         }
     }
 }
