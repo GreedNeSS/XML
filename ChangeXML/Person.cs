e68a66e83f4b3345ac1c912051d67d665995c9db
GreedNeSS <ruslan199126@gmail.com>
2022-03-14 18:14:23 +0300

    Added XML change example.

---
+++ ChangeXML/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeXML
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Company { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, int age, string company)
+        {
+            Name = name;
+            Age = age;
+            Company = company;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Company: {Company}";
+        }
+    }
+}
---
+++ ChangeXML/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Xml;
+
+namespace ChangeXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Change XML *****");
+
+            string path = @"People.xml";
+
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(path);
+            XmlElement? root = xDoc.DocumentElement;
+
+            if (root != null)
+            {
+                XmlElement newPeople = xDoc.CreateElement("people");
+
+                XmlAttribute nameAtr = xDoc.CreateAttribute("name");
+                XmlElement ageEl = xDoc.CreateElement("age");
+                XmlElement companyEl = xDoc.CreateElement("company");
+
+                XmlText nameText = xDoc.CreateTextNode("GreeD");
+                XmlText ageText = xDoc.CreateTextNode("30");
+                XmlText companyText = xDoc.CreateTextNode("Microsoft");
+
+                nameAtr.AppendChild(nameText);
+                ageEl.AppendChild(ageText);
+                companyEl.AppendChild(companyText);
+
+                newPeople.Attributes.Append(nameAtr);
+                newPeople.AppendChild(ageEl);
+                newPeople.AppendChild(companyEl);
+
+                root.AppendChild(newPeople);
+
+                XmlNode? first = root?.FirstChild;
+                if (first is not null)
+                {
+                    root?.RemoveChild(first);
+                }
+
+                xDoc.Save(path);
+            }
+        }
+    }
+}
